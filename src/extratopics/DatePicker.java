63780cd2e198db94018fda099c7b24d4f6170bda
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2025-09-17 22:15:42 +0530

    Selenium Season 2 notes and programs till Sep 17th

--- src/extratopics/DatePicker.java
+++ src/extratopics/DatePicker.java
@@ -35,20 +35,22 @@ public class DatePicker {
 		driver.findElement(By.xpath("//div[@data-testid='departure-date-dropdown-label-test-id']")).click();
 		
 		
-		String targetDay="15";
-		String targetMonth="January";
+		String targetDay="10";
+		String targetMonth="April";
 		String targetYear="2026";
-		
+		int firstIndex=1;
+		int secondIndex=2;
 		while(true)
 		{
 			
-		WebElement monthYearElement1=driver.findElement(By.xpath("(//div[contains(@data-testid,'undefined-month-')])[1]/descendant::div[2]"));
+		WebElement monthYearElement1=driver.findElement(By.xpath("(//div[contains(@data-testid,'undefined-month-')])["+firstIndex+"]/descendant::div[2]"));
 		String displayedMonthYear1=monthYearElement1.getText();
 		
-		WebElement monthYearElement2=driver.findElement(By.xpath("(//div[contains(@data-testid,'undefined-month-')])[2]/descendant::div[2]"));
+		WebElement monthYearElement2=driver.findElement(By.xpath("(//div[contains(@data-testid,'undefined-month-')])["+secondIndex+"]/descendant::div[2]"));
 		String displayedMonthYear2=monthYearElement2.getText();
 		
-		if((displayedMonthYear1.contains(targetYear) && displayedMonthYear1.contains(targetMonth)))
+		if((displayedMonthYear1.contains(targetYear) && displayedMonthYear1.contains(targetMonth)) ||
+				(displayedMonthYear2.contains(targetYear) && displayedMonthYear2.contains(targetMonth)))
 		{
 			break;
 		}
@@ -56,6 +58,8 @@ public class DatePicker {
 		{
 			driver.findElement(By.xpath("(//*[name()='svg'])[18]")).click();
 			Thread.sleep(2000);
+			firstIndex=firstIndex+2;
+			secondIndex=secondIndex+2;
 		}
 			
 		}
